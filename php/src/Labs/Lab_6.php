aff5d3e60696ba621d5aad4f0aef572398066890
Sally <92568786+Sammy231004@users.noreply.github.com>
2022-11-10 13:42:26 +0700

    Lab_6

---
+++ php/src/Labs/Lab_6.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = [
+    'mobile phone ' => [
+        'samsung' => [
+            'adnroid'=>[
+                'samusng A50'
+            ]
+        ]
+    ]
+];
+
+$array['mobile phone ']['samsung']['android'][] = 'Samsung a 50';
